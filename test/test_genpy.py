3de5a3d7302c7ea299d25032b92171212d850dc3
[6~ <inform@tiker.net>
2020-05-17 13:00:37 -0500

    Add a basic test

---
+++ test/test_genpy.py
@@ -0,0 +1,58 @@
+from __future__ import division, absolute_import, print_function
+
+__copyright__ = "Copyright (C) 2020 Andreas Kloeckner"
+
+__license__ = """
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
+import genpy as gp
+import sys
+
+
+def test_basic_generation():
+    f = gp.Function(
+            "yoink", "x",
+            gp.If(
+                "x % 2 == 0",
+                gp.Suite([
+                    gp.Assign("y", "x+2"),
+                    gp.Return("y"),
+                    ]),
+                gp.Return("2*x"),
+                )
+            )
+
+    names = {}
+    exec(compile(str(f), "<generated>", 'exec'), names)
+
+    yoink = names["yoink"]
+
+    assert yoink(5) == 10
+    assert yoink(6) == 8
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        exec(sys.argv[1])
+    else:
+        from pytest import main
+        main([__file__])
+
+# vim: fdm=marker
