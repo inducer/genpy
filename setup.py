08615fcf424ab53f87649a62ef243ef0fe3c41a3
Matt Wala <wala1@illinois.edu>
2020-06-04 00:36:10 -0500

    Actually do those things

--- setup.py
+++ setup.py
@@ -1,36 +1,41 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-from setuptools import setup
+def main():
+    from setuptools import setup
 
-with open("README.rst", "rt") as inf:
-    readme = inf.read()
+    with open("README.rst", "rt") as inf:
+        readme = inf.read()
 
-setup(
-        name="genpy",
-        version="2016.1.3",
-        description="AST-based generation of Python source code",
-        long_description=readme,
-        classifiers=[
-            'Development Status :: 4 - Beta',
-            'Intended Audience :: Developers',
-            'Intended Audience :: Other Audience',
-            'Intended Audience :: Science/Research',
-            'License :: OSI Approved :: MIT License',
-            'Natural Language :: English',
-            'Programming Language :: Python',
-            'Topic :: Scientific/Engineering',
-            'Topic :: Software Development :: Libraries',
-            'Topic :: Utilities',
-            ],
+    version_dict = {}
+    init_filename = "boxtree/version.py"
+    exec(compile(open(init_filename, "r").read(), init_filename, "exec"),
+            version_dict)
 
-        author="Andreas Kloeckner",
-        author_email="inform@tiker.net",
-        license="MIT",
-        url="http://documen.tician.de/genpy/",
+    setup(
+            name="genpy",
+            version=version_dict["VERSION_TEXT"],
+            description="AST-based generation of Python source code",
+            long_description=readme,
+            classifiers=[
+                'Development Status :: 4 - Beta',
+                'Intended Audience :: Developers',
+                'Intended Audience :: Other Audience',
+                'Intended Audience :: Science/Research',
+                'License :: OSI Approved :: MIT License',
+                'Natural Language :: English',
+                'Programming Language :: Python',
+                'Topic :: Scientific/Engineering',
+                'Topic :: Software Development :: Libraries',
+                'Topic :: Utilities',
+                ],
 
-        packages=["genpy"],
-        install_requires=[
-            "pytools>=2015.1.2",
-            "numpy>=1.6",
-            ])
+            author="Andreas Kloeckner",
+            author_email="inform@tiker.net",
+            license="MIT",
+            url="http://documen.tician.de/genpy/",
+
+            packages=["genpy"],
+            install_requires=[
+                "pytools>=2015.1.2",
+                "numpy>=1.6",
+                ])
