baccb2ab64683e8790bde6efaecfd7cc9878797e
Alexandru Fikl <alexfikl@gmail.com>
2024-07-18 20:08:02 +0300

    ruff: fix issues

--- test/test_genpy.py
+++ test/test_genpy.py
@@ -20,8 +20,9 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
 
-import genpy as gp
 import sys
+
+import genpy as gp
 
 
 def test_basic_generation():
